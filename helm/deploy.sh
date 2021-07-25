55ffd46ea5a3d1dc4c655fbd42b62c9451d1f28f
couldntcatchthebus <ercansavas1@gmail.com>
2021-07-25 23:20:01 +0300

    added helm and ingress for that 2 services.

---
+++ helm/deploy.sh
@@ -0,0 +1,15 @@
+# Create a namespace for your ingress resources
+kubectl create namespace ingress-internal
+
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+# Use Helm to deploy an NGINX ingress controller
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-internal \
+    -f internal-ingress.yaml \
+    --set controller.replicaCount=2 \
+    --set controller.ingressClass=internal \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
